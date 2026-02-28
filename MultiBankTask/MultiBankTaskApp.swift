827e921b3d2437b948869c813c607de950be7450
Aleksei Bulatnikov <office@albustudio.com>
2026-02-28 11:06:31 +0200

    Set up Xcode project with initial iOS app structure

---
+++ MultiBankTask/MultiBankTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiBankTaskApp.swift
+//  MultiBankTask
+//
+//  Created by Alexey Bulatnikov on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MultiBankTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiBankTaskTests/MultiBankTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiBankTaskTests.swift
+//  MultiBankTaskTests
+//
+//  Created by Alexey Bulatnikov on 2/28/26.
+//
+
+import Testing
+@testable import MultiBankTask
+
+struct MultiBankTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
